b459322adef7aaadf616b58745d86e7912d08f37
boreddad <ronnebaumfinances@gmail.com>
2023-11-29 09:24:24 -0500

    go file reader

---
+++ go/internal/reader/reader.go
@@ -0,0 +1,13 @@
+package reader
+
+import (
+    "os"
+)
+
+func ReadFile(path string) (string, error) {
+    data, err := os.ReadFile(path)
+    if err != nil {
+        return "", err
+    }
+    return string(data), nil
+}
