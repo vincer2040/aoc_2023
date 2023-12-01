763806f85f6f0c8ec9a5b0039f3bae900ba48dc1
boreddad <ronnebaumfinances@gmail.com>
2023-12-01 10:53:08 -0500

    feat: rust day 1

---
+++ rs/src/day1.rs
@@ -0,0 +1,40 @@
+pub fn part_one(input: &str) {
+    let t = input.lines().into_iter().fold(0, |acc, l| {
+        let first = l.chars().find(|ch| {
+            '0' <= *ch && *ch <= '9'
+        }).unwrap();
+        let last = l.chars().rev().find(|ch| {
+            '0' <= *ch && *ch <= '9'
+        }).unwrap();
+        let t = format!("{}{}", first, last);
+        let res: i32 = t.parse().unwrap();
+        acc + res
+    });
+    println!("{}", t);
+}
+
+pub fn parse_two(input: &str) {
+    let t = input.lines().fold(0, |acc, l| {
+        println!("{}", l);
+        let mut s = l.replace("one", "one1one");
+        s = s.replace("two", "two2two");
+        s = s.replace("three", "three3three");
+        s = s.replace("four", "four4four");
+        s = s.replace("five", "five5five");
+        s = s.replace("six", "six6six");
+        s = s.replace("seven", "seven7seven");
+        s = s.replace("eight", "eight8eight");
+        s = s.replace("nine", "nine9nine");
+        println!("{}", s);
+        let first = s.chars().find(|ch| {
+            '0' <= *ch && *ch <= '9'
+        }).unwrap();
+        let last = s.chars().rev().find(|ch| {
+            '0' <= *ch && *ch <= '9'
+        }).unwrap();
+        let t = format!("{}{}", first, last);
+        let res: i32 = t.parse().unwrap();
+        acc + res
+    });
+    println!("{}", t);
+}
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+
+pub mod reader;
+pub mod day1;
+
+fn main() -> Result<(), std::io::Error> {
+    let input = reader::read(None)?;
+    day1::parse_two(&input);
+    Ok(())
 }
