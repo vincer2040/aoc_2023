eb614455f24e0135f563dd6267486f2ca94f06ad
boreddad <ronnebaumfinances@gmail.com>
2023-12-01 18:24:10 -0500

    chore: add tests

--- rs/src/day1.rs
+++ rs/src/day1.rs
@@ -1,4 +1,4 @@
-pub fn part_one(input: &str) {
+pub fn part_one(input: &str) -> i32 {
     let t = input.lines().into_iter().fold(0, |acc, l| {
         let first = l.chars().find(|ch| '0' <= *ch && *ch <= '9').unwrap();
         let last = l.chars().rev().find(|ch| '0' <= *ch && *ch <= '9').unwrap();
@@ -6,12 +6,11 @@ pub fn part_one(input: &str) {
         let res: i32 = t.parse().unwrap();
         acc + res
     });
-    println!("{}", t);
+    return t;
 }
 
-pub fn part_two(input: &str) {
+pub fn part_two(input: &str) -> i32 {
     let t = input.lines().fold(0, |acc, l| {
-        println!("{}", l);
         let mut s = l.replace("one", "one1one");
         s = s.replace("two", "two2two");
         s = s.replace("three", "three3three");
@@ -21,12 +20,40 @@ pub fn part_two(input: &str) {
         s = s.replace("seven", "seven7seven");
         s = s.replace("eight", "eight8eight");
         s = s.replace("nine", "nine9nine");
-        println!("{}", s);
         let first = s.chars().find(|ch| '0' <= *ch && *ch <= '9').unwrap();
         let last = s.chars().rev().find(|ch| '0' <= *ch && *ch <= '9').unwrap();
         let t = format!("{}{}", first, last);
         let res: i32 = t.parse().unwrap();
         acc + res
     });
-    println!("{}", t);
+    return t;
 }
+
+#[cfg(test)]
+mod test {
+    use super::{part_one, part_two};
+
+    #[test]
+    fn part_one_test() {
+        let input = r#"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"#;
+        let res = part_one(input);
+        assert_eq!(res, 142);
+    }
+
+    #[test]
+    fn parse_two_test() {
+        let input = r#"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"#;
+        let res = part_two(input);
+        assert_eq!(res, 281);
+    }
+}
+
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -4,6 +4,7 @@ pub mod day1;
 
 fn main() -> Result<(), std::io::Error> {
     let input = reader::read(None)?;
-    day1::part_two(&input);
+    let res = day1::part_two(&input);
+    println!("{}", res);
     Ok(())
 }
