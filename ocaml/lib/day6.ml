8b49395e8b91e458d0a44a0bd1fa036974d79104
boreddad <ronnebaumfinances@gmail.com>
2023-12-06 11:19:32 -0500

    feat: more optimal ocaml solution

--- ocaml/lib/day6.ml
+++ ocaml/lib/day6.ml
@@ -14,6 +14,14 @@ let replace str old new_sub =
   Re.replace_string re ~by:new_sub str
 ;;
 
+let get_possible_ways_to_win time dist =
+  let d = (time * time) - (4 * dist) in
+  let sqrt_d = int_of_float (sqrt (float_of_int d)) in
+  if sqrt_d * sqrt_d = d
+  then sqrt_d - 1
+  else sqrt_d + 1 - (time mod 2 lxor (sqrt_d mod 2))
+;;
+
 let p1 input =
   let split = String.split_on_char '\n' input |> List.filter filter_none in
   let times =
@@ -38,19 +46,8 @@ let p1 input =
   let result =
     List.fold_left
       (fun res race ->
-        let hold_times = List.init (race.time + 1) (fun i -> i) in
-        let race_time = race.time in
-        let record_distance = race.distance in
-        let num_ways_to_win =
-          List.fold_left
-            (fun acc hold_time ->
-              let remaining_time = race_time - hold_time in
-              let dist_travaled = hold_time * remaining_time in
-              if dist_travaled > record_distance then acc + 1 else acc)
-            0
-            hold_times
-        in
-        if num_ways_to_win > 0 then num_ways_to_win * res else res)
+        let possible = get_possible_ways_to_win race.time race.distance in
+        res * possible)
       1
       races
   in
@@ -73,15 +70,5 @@ let p2 input =
     |> List.fold_left (fun acc dist -> acc ^ dist) ""
     |> int_of_string
   in
-  let hold_times = List.init (time + 1) (fun i -> i) in
-  let result =
-    List.fold_left
-      (fun acc hold_time ->
-        let remaining_time = time - hold_time in
-        let dist_travaled = hold_time * remaining_time in
-        if dist_travaled > distance then acc + 1 else acc)
-      0
-      hold_times
-  in
-  result
+  get_possible_ways_to_win time distance
 ;;
