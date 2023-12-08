fc4ce84eba2a545fbf22b713f5dbf20378cf39cf
boreddad <ronnebaumfinances@gmail.com>
2023-12-07 19:28:46 -0500

    chore: add quick sort helper

---
+++ c/src/quick_sort.c
@@ -0,0 +1,55 @@
+#include "aoc.h"
+
+#define swap(a, b, size)                                                       \
+    do {                                                                       \
+        size_t x;                                                              \
+        unsigned char* uca = ((unsigned char*)a);                              \
+        unsigned char* ucb = ((unsigned char*)b);                              \
+        for (x = 0; x < size; ++x) {                                           \
+            unsigned char tmp = uca[x];                                        \
+            uca[x] = ucb[x];                                                   \
+            ucb[x] = tmp;                                                      \
+        }                                                                      \
+    } while (0)
+
+static ssize_t partition(void* arr, ssize_t lo, ssize_t hi, size_t data_size,
+                         CmpFn* fn) {
+    ssize_t idx = lo - 1;
+    unsigned char* uca = arr;
+    void *idx_val, *pivot = uca + (hi * data_size);
+    size_t i;
+
+    for (i = lo; i < hi; ++i) {
+        void* at = uca + (i * data_size);
+        int cmp = fn(at, pivot);
+        if (cmp <= 0) {
+            void* a;
+            idx++;
+            a = uca + (idx * data_size);
+            swap(a, at, data_size);
+        }
+    }
+
+    idx++;
+    idx_val = uca + (idx * data_size);
+    swap(idx_val, pivot, data_size);
+    return idx;
+}
+
+static void qs(void* arr, ssize_t lo, ssize_t hi, size_t data_size, CmpFn* fn) {
+    ssize_t pivot_idx;
+    while (lo < hi) {
+        pivot_idx = partition(arr, lo, hi, data_size, fn);
+        if ((pivot_idx - lo) < (hi - pivot_idx)) {
+            qs(arr, lo, pivot_idx - 1, data_size, fn);
+            lo = pivot_idx + 1;
+        } else {
+            qs(arr, pivot_idx + 1, hi, data_size, fn);
+            hi = pivot_idx - 1;
+        }
+    }
+}
+
+void quick_sort(void* arr, size_t len, size_t data_size, CmpFn* fn) {
+    qs(arr, 0, len - 1, data_size, fn);
+}
