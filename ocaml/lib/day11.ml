a34395be2e0a24665738f2653da072a4868d7207
boreddad <ronnebaumfinances@gmail.com>
2023-12-11 13:14:01 -0500

    perf: have optimal solution for p1

--- ocaml/lib/day11.ml
+++ ocaml/lib/day11.ml
@@ -6,68 +6,6 @@ let filter_none line =
   | _ -> true
 ;;
 
-let rec splice_list lst idx item =
-  match lst, idx with
-  | [], i when i > 0 -> failwith "Index out of bounds"
-  | [], _ -> [ item ]
-  | hd :: tl, i when i > 0 -> hd :: splice_list tl (i - 1) item
-  | _, 0 -> item :: lst
-  | _, _ -> failwith "Index out of bounds"
-;;
-
-let split_on_idx str idx =
-  let str_len = String.length str in
-  if idx < 0 || idx >= str_len
-  then failwith "Index out of bounds"
-  else (
-    let prefix = String.sub str 0 idx in
-    let suffix = String.sub str idx (str_len - idx) in
-    prefix, suffix)
-;;
-
-let rec expand_rows solar_system idx =
-  let create_new_galaxy () =
-    let row_len = String.length (List.nth solar_system 0) in
-    let new_str = String.make row_len '.' in
-    let new_galaxy = splice_list solar_system idx new_str in
-    new_galaxy
-  in
-  if idx >= List.length solar_system
-  then solar_system
-  else (
-    let row = List.nth solar_system idx in
-    let set =
-      String.fold_left (fun acc char -> CharSet.add char acc) CharSet.empty row
-    in
-    if CharSet.cardinal set == 1
-    then expand_rows (create_new_galaxy ()) (idx + 2)
-    else expand_rows solar_system (idx + 1))
-;;
-
-let rec expand_columns solar_system idx =
-  let row_len = String.length (List.nth solar_system 0) in
-  let create_new_galaxy () =
-    let add_to_galaxy row =
-      let prefix, suffix = split_on_idx row idx in
-      let new_row = prefix ^ "." ^ suffix in
-      new_row
-    in
-    let new_galaxy = List.map add_to_galaxy solar_system in
-    new_galaxy
-  in
-  if idx >= row_len
-  then solar_system
-  else (
-    let add_to_set acc row =
-      let ch = String.get row idx in
-      CharSet.add ch acc
-    in
-    let set = List.fold_left add_to_set CharSet.empty solar_system in
-    if CharSet.cardinal set == 1
-    then expand_columns (create_new_galaxy ()) (idx + 2)
-    else expand_columns solar_system (idx + 1))
-;;
-
 let rec get_row_expansions solar_system idx num_expansions expansions =
   if idx >= List.length solar_system
   then List.rev expansions
@@ -142,41 +80,7 @@ let manhatten (y1, x1) (y2, x2) =
   a + b
 ;;
 
-let num_times_to_expand = 1_000_000
-
-let p1 input =
-  let solar_system =
-    String.split_on_char '\n' input |> List.filter filter_none
-  in
-  let expanded = expand_columns (expand_rows solar_system 0) 0 in
-  let galaxy_coords = find_galaxies expanded 0 [] in
-  let map = Hashtbl.create 10 in
-  let create_list i =
-    List.init (List.length galaxy_coords - i - 1) (fun x -> i + x + 1)
-  in
-  let add_point acc idx =
-    let p = List.nth galaxy_coords idx in
-    p :: acc
-  in
-  List.iteri
-    (fun i (y, x) ->
-      let l = create_list i in
-      let to_points = List.fold_left add_point [] l |> List.rev in
-      Hashtbl.add map (y, x) to_points)
-    galaxy_coords;
-  let get_dists (y, x) value acc =
-    let get_dist acc (yi, xi) =
-      let d = manhatten (y, x) (yi, xi) in
-      acc + d
-    in
-    let dists = List.fold_left get_dist 0 value in
-    acc + dists
-  in
-  let res = Hashtbl.fold get_dists map 0 in
-  res
-;;
-
-let p2 input =
+let solve input num_times =
   let solar_system =
     String.split_on_char '\n' input |> List.filter filter_none
   in
@@ -188,14 +92,14 @@ let p2 input =
       let row_exp = List.nth row_expansions y in
       let col_exp = List.nth col_expansions x in
       let new_y =
-        if num_times_to_expand == 1
-        then y + (row_exp * num_times_to_expand)
-        else y + (row_exp * (num_times_to_expand - 1))
+        if num_times == 1
+        then y + (row_exp * num_times)
+        else y + (row_exp * (num_times - 1))
       in
       let new_x =
-        if num_times_to_expand == 1
-        then x + (col_exp * num_times_to_expand)
-        else x + (col_exp * (num_times_to_expand - 1))
+        if num_times == 1
+        then x + (col_exp * num_times)
+        else x + (col_exp * (num_times - 1))
       in
       new_y, new_x)
   in
@@ -224,3 +128,6 @@ let p2 input =
   let res = Hashtbl.fold get_dists map 0 in
   res
 ;;
+
+let p1 input = solve input 1
+let p2 input = solve input 1_000_000
